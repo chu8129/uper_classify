0ced578d141c3669f40df0ce4f05bee032688862
wangqiuwen <815234899@qq.com>
2018-09-09 16:51:26 +0800

    ~

---
+++ config/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# qw @ 2018-09-09 13:41:09
+
+import sys
+sys.path.append("./")
+
+COLUMN_SPLIT = "`|"
+ITEM_SPLIT = "`|"
+
+WORD_TO_VECTOR_MODEL_FILE_PATH = "data/cn.skipgram.bin"
